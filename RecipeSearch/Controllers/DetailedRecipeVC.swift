e683f8b32dd06407786a11d3c9d6ba1e54025007
David <lin.david985@gmail.com>
2019-12-15 18:16:12 -0500

    created detailed VC for recipe (directions and additional info)

---
+++ RecipeSearch/Controllers/DetailedRecipeVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailedRecipeVC.swift
+//  RecipeSearch
+//
+//  Created by David Lin on 12/15/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailedRecipeVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RecipeSearch/Controllers/DetailedRecipeVCViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedRecipeVCViewController.swift
+//  RecipeSearch
+//
+//  Created by David Lin on 12/15/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailedRecipeVCViewController: UIViewController {
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeDirectionText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
